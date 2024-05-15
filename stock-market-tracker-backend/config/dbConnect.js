35991b111dae787a92baebb7cfdacb40e062f648
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-05-15 21:07:52 +0530

    completed

--- stock-market-tracker-backend/config/dbConnect.js
+++ stock-market-tracker-backend/config/dbConnect.js
@@ -1,7 +1,8 @@
 const {default:mongoose}=require("mongoose");
+let uri=`mongodb+srv://priyanshus20k4:Priyanshu%40834@mycluster.dnwjrp2.mongodb.net/Stocks?retryWrites=true&w=majority&appName=MyCluster`;
 const dbConnect=()=>{
     try {
-        const conn=mongoose.connect(process.env.MONGODB_URL)
+        const conn=mongoose.connect(uri);
         console.log("Database connect");
     } catch (error) {
         throw new Error(error);
