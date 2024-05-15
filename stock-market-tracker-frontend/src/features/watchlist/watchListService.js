af3c5d61b6430f5fc15ace5df0f8196864b7aca0
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-05-15 21:24:04 +0530

    changes

--- stock-market-tracker-frontend/src/features/watchlist/watchListService.js
+++ stock-market-tracker-frontend/src/features/watchlist/watchListService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { base_url } from "../../utils/base_url"
 const addToWatchList=async(data)=>{
-    const response=await axios.put(`${base_url}user/watchlist`,{title:data.title},data.config2);
+    const response=await axios.put(`${base_url}watchlist`,{title:data.title},data.config2);
     if(response?.data){
         return response?.data;
     }
 }
 const getWatchList=async(data)=>{
-    const response=await axios.get(`${base_url}user/watchlist`,data);
+    const response=await axios.get(`${base_url}watchlist`,data);
     return response?.data;
 }
 const watchListService={
