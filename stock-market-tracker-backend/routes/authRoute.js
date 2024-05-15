6316835eb155266dcae26ec5cceef83a4ccf906f
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-05-15 22:05:34 +0530

    ch

--- stock-market-tracker-backend/routes/authRoute.js
+++ stock-market-tracker-backend/routes/authRoute.js
@@ -9,8 +9,8 @@ const { authMiddleware } = require("../middlewares/authMiddlewares");
 
 const router = express.Router();
 //authroute->userctrl->usermodel tocheck user
-router.post("/login", loginUserCtrl);
-router.post("/register", createUser);
-router.put("/watchlist",authMiddleware,addToWatchList);
-router.get("/watchlist",authMiddleware,getWatchList);
+router.post("login", loginUserCtrl);
+router.post("register", createUser);
+router.put("watchlist",authMiddleware,addToWatchList);
+router.get("watchlist",authMiddleware,getWatchList);
 module.exports = router;
